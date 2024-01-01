dd1ee2a434a5eafe28633fa3e1dab3f7fcac2d9a
YTB2024 <2917553892@qq.com>
2024-01-01 14:51:29 +0800

    初始化代码

--- src/types/interface.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @description User-Service parameters
- */
-export interface IUserOptions {
-  account: string;
-  password: string;
-}
--- src/user/dto/sign.dto.ts
+++
@@ -1,6 +0,0 @@
-export type LoginResponse = {
-  error: string;
-  url: null;
-  goto2: string;
-  msg?: string;
-};
