e91f81497ce6794d738e45696fb3ad424dda289b
QinLoong <2722937531@qq.com>
2024-01-04 16:14:35 +0800

    导入自定义的UtilService

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -1,2 +1,3 @@
 import { Global, Module } from '@nestjs/common'; // 导入NestJS的Global装饰器和Module装饰器
 import { HttpService } from './service/impl/http.service'; // 导入自定义的HttpService
+import { UtilService } from './service/impl/util.service'; // 导入自定义的UtilService
