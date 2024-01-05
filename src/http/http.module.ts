5c27909cca699ad0265406fcacf5a2d3a7d03764
QinLoong <2722937531@qq.com>
2024-01-05 16:16:22 +0800

    使用Global装饰器

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -1,3 +1,4 @@
 import { Global, Module } from '@nestjs/common'; // 导入NestJS的Global装饰器和Module装饰器
 import { HttpService } from './service/impl/http.service'; // 导入自定义的HttpService
 import { UtilService } from './service/impl/util.service'; // 导入自定义的UtilService
+@Global() // 使用Global装饰器，将该模块标记为全局模块，可以在整个应用程序中访问其提供的服务
