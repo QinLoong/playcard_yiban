2ca25f7ae0cb2a3c5ecc6e50f5810d6dea418b46
QinLoong <2722937531@qq.com>
2024-01-13 16:30:22 +0800

    导入UserModule模块

--- src/index/index.module.ts
+++ src/index/index.module.ts
@@ -1,3 +1,4 @@
 import { Module } from '@nestjs/common';
 import { IndexController } from './controller/index.controller';
 import { IndexService } from './service/impl/index.service';
+import { UserModule } from '../user/user.module';
