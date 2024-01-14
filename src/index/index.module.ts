feca78c64cddb7cacfb978fb257105f67c53d50e
QinLoong <2722937531@qq.com>
2024-01-14 16:30:57 +0800

    导入HttpModule模块

--- src/index/index.module.ts
+++ src/index/index.module.ts
@@ -2,3 +2,4 @@ import { Module } from '@nestjs/common';
 import { IndexController } from './controller/index.controller';
 import { IndexService } from './service/impl/index.service';
 import { UserModule } from '../user/user.module';
+import { HttpModule } from '../http/http.module';
