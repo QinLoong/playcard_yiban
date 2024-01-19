b1a9c9f8a9009f15bfc71892d9fc1afed4bfc5db
QinLoong <2722937531@qq.com>
2024-01-19 16:34:51 +0800

    导入NestJS Schedule模块中的SchedulerRegistry

--- src/index/controller/index.controller.ts
+++ src/index/controller/index.controller.ts
@@ -1,1 +1,2 @@
 import { Controller, Get, Logger } from '@nestjs/common'; // 导入NestJS的Controller装饰器、Get装饰器和Logger
+import { SchedulerRegistry } from '@nestjs/schedule'; // 导入NestJS Schedule模块中的SchedulerRegistry
