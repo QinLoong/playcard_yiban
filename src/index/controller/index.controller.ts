52f3f0d9724843f43e4a26ee207299591160159c
QinLoong <2722937531@qq.com>
2024-01-20 16:35:13 +0800

    导入自定义的IndexService

--- src/index/controller/index.controller.ts
+++ src/index/controller/index.controller.ts
@@ -1,2 +1,3 @@
 import { Controller, Get, Logger } from '@nestjs/common'; // 导入NestJS的Controller装饰器、Get装饰器和Logger
 import { SchedulerRegistry } from '@nestjs/schedule'; // 导入NestJS Schedule模块中的SchedulerRegistry
+import { IndexService } from '../service/impl/index.service'; // 导入自定义的IndexService
