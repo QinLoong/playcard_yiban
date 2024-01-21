03f46d7eb05acc80fc61d63d8898f70fdc81ce01
QinLoong <2722937531@qq.com>
2024-01-21 16:35:39 +0800

    使用Controller装饰器，将该类标记为NestJS的控制器类

--- src/index/controller/index.controller.ts
+++ src/index/controller/index.controller.ts
@@ -1,3 +1,4 @@
 import { Controller, Get, Logger } from '@nestjs/common'; // 导入NestJS的Controller装饰器、Get装饰器和Logger
 import { SchedulerRegistry } from '@nestjs/schedule'; // 导入NestJS Schedule模块中的SchedulerRegistry
 import { IndexService } from '../service/impl/index.service'; // 导入自定义的IndexService
+@Controller() // 使用Controller装饰器，将该类标记为NestJS的控制器类
