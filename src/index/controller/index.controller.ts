eaa999cb377ed180a0e868b051d803168533be6c
QinLoong <2722937531@qq.com>
2024-01-22 16:36:29 +0800

    创建并导出IndexController类

--- src/index/controller/index.controller.ts
+++ src/index/controller/index.controller.ts
@@ -2,3 +2,5 @@ import { Controller, Get, Logger } from '@nestjs/common'; // 导入NestJS的Cont
 import { SchedulerRegistry } from '@nestjs/schedule'; // 导入NestJS Schedule模块中的SchedulerRegistry
 import { IndexService } from '../service/impl/index.service'; // 导入自定义的IndexService
 @Controller() // 使用Controller装饰器，将该类标记为NestJS的控制器类
+export class IndexController {
+}
