617be5aec95ec15d276e58a25ae2e64951782106
QinLoong <2722937531@qq.com>
2024-01-24 16:37:35 +0800

    注入IndexService实例，用于处理业务逻辑

--- src/index/controller/index.controller.ts
+++ src/index/controller/index.controller.ts
@@ -4,4 +4,8 @@ import { IndexService } from '../service/impl/index.service'; // 导入自定义
 @Controller() // 使用Controller装饰器，将该类标记为NestJS的控制器类
 export class IndexController {
     private readonly logger = new Logger(IndexController.name); // 创建一个Logger实例，用于记录日志信息
+    constructor(
+        private readonly appService: IndexService, // 注入IndexService实例，用于处理业务逻辑
+        
+      ) {}
 }
