02e7a65d3ae6861d602cd028b8d0cd924de72dc1
QinLoong <2722937531@qq.com>
2024-02-11 17:11:39 +0800

    创建一个类型，表示signFormData的字段名称

--- src/index/dto/signFormData2DailySignSubmitRequestDTOMapping.ts
+++ src/index/dto/signFormData2DailySignSubmitRequestDTOMapping.ts
@@ -1,2 +1,4 @@
 import { DailySignSubmitRequest } from '../../user/dto/dailySignSubmit.dto'; // 导入DailySignSubmitRequest类型
 import { signFormData } from './signConfigDTO'; // 导入signFormData类型
+// 创建一个类型，表示signFormData的字段名称
+type signFormDataKey = keyof signFormData;
