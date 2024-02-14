f1e42ba0ed81acfb32795ceee48d734811e9300c
QinLoong <2722937531@qq.com>
2024-02-14 17:13:17 +0800

    创建一个映射，将signFormData字段映射到DailySignSubmitRequest字段

--- src/index/dto/signFormData2DailySignSubmitRequestDTOMapping.ts
+++ src/index/dto/signFormData2DailySignSubmitRequestDTOMapping.ts
@@ -10,3 +10,17 @@ type DailySignSubmitRequestKey = keyof DailySignSubmitRequest;
 export type SignConfig2DailySignSubmitRequestDTOMappingType = {
     [k in signFormDataKey]: DailySignSubmitRequestKey;
   };
+
+// 创建一个映射，将signFormData字段映射到DailySignSubmitRequest字段
+export const SignFormData2DailySignSubmitRequestDTOMapping: SignConfig2DailySignSubmitRequestDTOMappingType =
+  {
+    address: 'dkdz',
+    position: 'dkdzZb',
+    signAddress: 'dkd',
+    provinceZipCode: 'jzdSheng.dm',
+    cityZipCode: 'jzdShi.dm',
+    countyZipCode: 'jzdXian.dm',
+    habitationDetailDesc: 'jzdDz',
+    usualResidenceDetailDesc: 'jzdDz2',
+    phone: 'lxdh',
+  };
