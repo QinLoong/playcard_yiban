42bca91d32183e9e0c846785de7c85df8f861176
QinLoong <2722937531@qq.com>
2024-02-21 17:21:47 +0800

    导入用户选项接口

--- src/index/service/index.service.ts
+++ src/index/service/index.service.ts
@@ -1,2 +1,3 @@
 import { Injectable, Logger } from '@nestjs/common'; // 导入NestJS的Injectable装饰器和Logger
 import { Cron } from '@nestjs/schedule'; // 导入NestJS Schedule模块中的Cron装饰器
+import { IUserOptions } from 'src/types/interface'; // 导入用户选项接口
