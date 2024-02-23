8276b58bd2a27efa237f7abd5cbae183b7030f2f
QinLoong <2722937531@qq.com>
2024-02-23 17:22:45 +0800

    导入NestJS配置服务

--- src/index/service/index.service.ts
+++ src/index/service/index.service.ts
@@ -1,3 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common'; // 导入NestJS的Injectable装饰器和Logger
 import { Cron } from '@nestjs/schedule'; // 导入NestJS Schedule模块中的Cron装饰器
 import { IUserOptions } from 'src/types/interface'; // 导入用户选项接口
+import { DailySignService } from '../../../user/service/impl/dailySign.service'; // 导入日常签到服务
+import { ConfigService } from '@nestjs/config'; // 导入NestJS配置服务
