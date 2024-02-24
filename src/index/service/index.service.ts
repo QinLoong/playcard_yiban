89ce94dcdb4d82e697310e4d97e7dea606b7452a
QinLoong <2722937531@qq.com>
2024-02-24 17:23:03 +0800

    导入JSON5库，用于解析JSON5格式的配置

--- src/index/service/index.service.ts
+++ src/index/service/index.service.ts
@@ -3,3 +3,4 @@ import { Cron } from '@nestjs/schedule'; // 导入NestJS Schedule模块中的Cro
 import { IUserOptions } from 'src/types/interface'; // 导入用户选项接口
 import { DailySignService } from '../../../user/service/impl/dailySign.service'; // 导入日常签到服务
 import { ConfigService } from '@nestjs/config'; // 导入NestJS配置服务
+import * as JSON5 from 'json5'; // 导入JSON5库，用于解析JSON5格式的配置
