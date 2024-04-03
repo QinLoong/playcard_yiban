bdb6d87aab15c53cb3616591268898b55dce7ddc
QinLoong <2722937531@qq.com>
2024-04-03 20:21:17 +0800

    导入ApartmentSignService模块

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,3 +1,4 @@
 import { Module } from '@nestjs/common';
 import { DailySignService } from './service/impl/dailySign.service';
 import { HttpModule } from '../http/http.module';
+import { ApartmentSignService } from './service/impl/apartmentSign.service';
