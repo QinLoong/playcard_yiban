1f5f4813e2a9d7fdb2613e4ac7759e9cdc474bc1
QinLoong <2722937531@qq.com>
2024-04-06 20:22:39 +0800

    导入DailySignApiService模块

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,3 +4,4 @@ import { HttpModule } from '../http/http.module';
 import { ApartmentSignService } from './service/impl/apartmentSign.service';
 import { SignApiService } from './api/signApi.service';
 import { ApartmentSignApiService } from './api/apartmentSignApi.service';
+import { DailySignApiService } from './api/dailySignApi.service';
