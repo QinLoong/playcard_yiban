f5d0c09abe10267c68db6e9f62232c5706df6aaf
QinLoong <2722937531@qq.com>
2024-04-13 20:55:02 +0800

    导入任务范围请求和响应数据类型

--- src/user/api/apartmentSignApi.service.ts
+++ src/user/api/apartmentSignApi.service.ts
@@ -1,3 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common'; // 导入NestJS的Injectable装饰器和Logger
 import { HttpService } from 'src/http/service/impl/http.service'; // 导入自定义的HttpService
 import { ApartmentSignApiEnum } from '../constant/dailySignApiEnum'; // 导入公寓签到API枚举
+import {
+  getTaskScopeRequest,
+  TaskScopeRequest,
+  TaskScopeResponse,
+} from '../dto/taskScope.dto'; // 导入任务范围请求和响应数据类型
