d86e7cfe3c7faf8d7ccf35d35dd6699248f856f9
QinLoong <2722937531@qq.com>
2024-04-28 21:06:39 +0800

    导入日常签到API枚举

--- src/user/api/dailySignApi.service.ts
+++ src/user/api/dailySignApi.service.ts
@@ -1,3 +1,4 @@
 import { Injectable, Logger } from '@nestjs/common'; // 导入NestJS的Injectable装饰器和Logger
 import { HttpService } from '../../http/service/impl/http.service'; // 导入自定义的HttpService
+import { DailySignApiEnum } from '../constant/dailySignApiEnum'; // 导入日常签到API枚举
 
