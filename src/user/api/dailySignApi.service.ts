e05c23eb6a03dcafe91d124e118a93ec0ec13121
QinLoong <2722937531@qq.com>
2024-04-29 21:06:53 +0800

    导入form-urlencoded库，用于处理表单数据编码

--- src/user/api/dailySignApi.service.ts
+++ src/user/api/dailySignApi.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common'; // 导入NestJS的Injectable装饰器和Logger
 import { HttpService } from '../../http/service/impl/http.service'; // 导入自定义的HttpService
 import { DailySignApiEnum } from '../constant/dailySignApiEnum'; // 导入日常签到API枚举
+import * as formurlencoded from 'form-urlencoded'; // 导入form-urlencoded库，用于处理表单数据编码
 
