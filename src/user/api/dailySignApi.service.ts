e80d4ef216c075bd30cffee8cdd24369bf56b91b
QinLoong <2722937531@qq.com>
2024-04-30 21:07:35 +0800

    导入日常签到提交请求和响应数据类型

--- src/user/api/dailySignApi.service.ts
+++ src/user/api/dailySignApi.service.ts
@@ -2,4 +2,10 @@ import { Injectable, Logger } from '@nestjs/common'; // 导入NestJS的Injectabl
 import { HttpService } from '../../http/service/impl/http.service'; // 导入自定义的HttpService
 import { DailySignApiEnum } from '../constant/dailySignApiEnum'; // 导入日常签到API枚举
 import * as formurlencoded from 'form-urlencoded'; // 导入form-urlencoded库，用于处理表单数据编码
+import {
+  DailySignSubmitRequest,
+  DailySignSubmitResponse,
+  SignLogRequest,
+  SignLogResponse,
+} from '../dto/dailySignSubmit.dto'; // 导入日常签到提交请求和响应数据类型
 
