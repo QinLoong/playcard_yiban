347c01313cdb982796ca281e2345b34abed31e7b
QinLoong <2722937531@qq.com>
2024-05-10 21:12:26 +0800

    导入登录响应数据类型

--- src/user/api/signApi.service.ts
+++ src/user/api/signApi.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common'; // 导入NestJS的Injectable装饰器和Logger
 import { HttpService } from '../../http/service/impl/http.service'; // 导入自定义的HttpService
-
+import { LoginResponse } from '../dto/sign.dto'; // 导入登录响应数据类型
 
 
