5b1f2f37c0dc60c51250d41e078a128ba5ad5743
QinLoong <2722937531@qq.com>
2024-05-11 21:13:14 +0800

    使用NestJS的Injectable装饰器，将该类标记为一个可注入的服务

--- src/user/api/signApi.service.ts
+++ src/user/api/signApi.service.ts
@@ -2,4 +2,5 @@ import { Injectable, Logger } from '@nestjs/common'; // 导入NestJS的Injectabl
 import { HttpService } from '../../http/service/impl/http.service'; // 导入自定义的HttpService
 import { LoginResponse } from '../dto/sign.dto'; // 导入登录响应数据类型
 
+@Injectable() // 使用NestJS的Injectable装饰器，将该类标记为一个可注入的服务
 
