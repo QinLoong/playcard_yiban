272208076284fffa4eb669a917580b4e77599bca
QinLoong <2722937531@qq.com>
2024-05-12 21:13:27 +0800

    创建一个Logger实例，用于记录日志信息

--- src/user/api/signApi.service.ts
+++ src/user/api/signApi.service.ts
@@ -3,4 +3,7 @@ import { HttpService } from '../../http/service/impl/http.service'; // 导入自
 import { LoginResponse } from '../dto/sign.dto'; // 导入登录响应数据类型
 
 @Injectable() // 使用NestJS的Injectable装饰器，将该类标记为一个可注入的服务
+export class SignApiService {
+  private readonly logger = new Logger(); // 创建一个Logger实例，用于记录日志信息
 
+}
