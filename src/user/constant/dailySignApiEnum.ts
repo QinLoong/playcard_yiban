dafa82df8d0d8761818fd357b7df041ffdaebd4a
QinLoong <2722937531@qq.com>
2024-05-18 21:16:08 +0800

    导出类DailySignApiEnum

--- src/user/constant/dailySignApiEnum.ts
+++ src/user/constant/dailySignApiEnum.ts
@@ -1,2 +1,10 @@
 export const dailySignApiPrefix = 'http://xggl.hnie.edu.cn';
+export class DailySignApiEnum {
+  static indexPageUrl = `${dailySignApiPrefix}/wap/main/welcome`;
+  static loginApi = `${dailySignApiPrefix}/website/login`;
+  static signLogApi = `${dailySignApiPrefix}/content/tabledata/student/temp/zzdk`;
+  static signPageApi = `${dailySignApiPrefix}/wap/menu/student/temp/zzdk/_child_/edit`;
+  static patchSignApi = `${dailySignApiPrefix}/content/student/temp/zzdk`;
+}
+
 
