79d62e85b6ec668fcaeb1b922c949ef3896fc906
QinLoong <2722937531@qq.com>
2024-05-27 21:20:45 +0800

    签到ID

--- src/user/dto/apartmentSignSubmit.dto.ts
+++ src/user/dto/apartmentSignSubmit.dto.ts
@@ -2,5 +2,32 @@ export type SubmitSignResponse = {
     result: boolean;
     msg: string;
   };
+  export type SubmitSignRequest = {
+    pathFile: string;
+    /**
+     * 签到ID
+     * @example 16632903199821049443
+     */
+    dm: string;
 
+  };
+  
+  export function getSubmitSignRequest(
+    obj: Partial<SubmitSignRequest>,
+  ): SubmitSignRequest {
+    return Object.assign(
+      {
+        pathFile: '',
+        dm: '',
+        sjdm: '',
+        zb: '112.944,27.8296',
+        wz: '',
+        ly: 'yiban',
+        qdwzZt: '',
+        fwwDistance: '',
+        operationType: 'Update',
+      },
+      obj,
+    );
+  }
   
