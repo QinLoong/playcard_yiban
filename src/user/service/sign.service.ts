d3ee080385ff2ef2b703a982de71b13abdcbcc2c
QinLoong <2722937531@qq.com>
2024-06-20 21:31:32 +0800

    导入登录API服务

--- src/user/service/sign.service.ts
+++ src/user/service/sign.service.ts
@@ -1,5 +1,6 @@
 import { HttpService } from '../../http/service/impl/http.service'; // 导入HttpService
 import { IUserOptions } from '../../types/interface'; // 导入用户选项接口
+import { SignApiService } from '../api/signApi.service'; // 导入登录API服务
 
 
 
