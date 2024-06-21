120049565409c10bafb2d1b9e94a4e8644c47f74
QinLoong <2722937531@qq.com>
2024-06-21 21:31:43 +0800

    导入CryptoJS库

--- src/user/service/sign.service.ts
+++ src/user/service/sign.service.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '../../http/service/impl/http.service'; // 导入HttpService
 import { IUserOptions } from '../../types/interface'; // 导入用户选项接口
 import { SignApiService } from '../api/signApi.service'; // 导入登录API服务
+import * as CryptoJS from 'crypto-js/core'; // 导入CryptoJS库
 
 
 
