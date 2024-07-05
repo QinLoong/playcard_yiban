6512aceb7afded3e311071df53118f4522b59943
QinLoong <2722937531@qq.com>
2024-07-05 21:35:55 +0800

    导入公寓签到API服务

--- src/user/service/impl/apartmentSign.service.ts
+++ src/user/service/impl/apartmentSign.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common'; // 导入NestJS的Injectable装饰器和Logger
 import { SignService } from '../sign.service'; // 导入SignService
 import { ApartmentSignApiEnum } from '../../constant/dailySignApiEnum'; // 导入公寓签到API枚举
+import { ApartmentSignApiService } from '../../api/apartmentSignApi.service'; // 导入公寓签到API服务
 
