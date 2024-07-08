c674fbaeb275bfd04193191b4ae343ec7b8a59c8
QinLoong <2722937531@qq.com>
2024-07-08 21:36:43 +0800

    公寓签到登录API的URL

--- src/user/service/impl/apartmentSign.service.ts
+++ src/user/service/impl/apartmentSign.service.ts
@@ -10,3 +10,10 @@ import {
 } from '../../dto/apartmentSignSubmit.dto'; // 导入提交晚归签到请求和响应数据类型
 
 @Injectable() // 使用NestJS的Injectable装饰器，将该类标记为一个可注入的服务
+export class ApartmentSignService extends SignService {
+  private readonly logger = new Logger(ApartmentSignService.name); // 创建一个Logger实例，用于记录日志信息
+
+  protected indexPageUrl = ApartmentSignApiEnum.indexPageUrl; // 公寓签到首页的URL
+  protected loginApi = ApartmentSignApiEnum.loginApi; // 公寓签到登录API的URL
+
+}
