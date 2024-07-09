92c0c070cc4fdc7b8215f9e771332f8291fc77e5
QinLoong <2722937531@qq.com>
2024-07-09 21:37:23 +0800

    晚归打卡

--- src/user/service/impl/apartmentSign.service.ts
+++ src/user/service/impl/apartmentSign.service.ts
@@ -16,4 +16,45 @@ export class ApartmentSignService extends SignService {
   protected indexPageUrl = ApartmentSignApiEnum.indexPageUrl; // 公寓签到首页的URL
   protected loginApi = ApartmentSignApiEnum.loginApi; // 公寓签到登录API的URL
 
+  constructor(
+    protected readonly httpService: HttpService,
+    protected readonly signApiService: SignApiService,
+    protected readonly apartmentSignApiService: ApartmentSignApiService,
+  ) {
+    super(httpService, signApiService);
+  }
+
+  /**
+   * 晚归打卡
+   * @param param 部分提交晚归签到的请求参数
+   */
+  async doSign(param: Partial<SubmitSignRequest>) {
+    const isAvailable = await this.apartmentSignApiService.isAvailableSign();
+    if (!isAvailable) {
+      return { result: true, message: '已经打卡完成' };
+    }
+    const signList = await this.apartmentSignApiService.getSignList();
+    if (signList.aaData.length === 0 || signList.aaData[0].VALID === '0') {
+      return { result: true, message: '暂无可用的晚归打卡' };
+    }
+    const signConfig = signList.aaData.shift();
+    const taskId = signConfig.DM; // 获取任务ID
+    const sjdm = signConfig.SJDM; // 获取时间段ID
+    const res = await this.apartmentSignApiService.getTaskScope(taskId); // 获取任务范围
+    const pos = res[0].position; // 获取位置坐标
+    const posText = res[0].mc; // 获取位置描述
+    const result = await this.apartmentSignApiService.submitSign(
+      getSubmitSignRequest({
+        zb: pos, // 坐标
+        sjdm, // 时间段ID
+        dm: taskId, // 任务ID
+        wz: posText, // 位置描述
+        ...param, // 部分请求参数
+      }),
+    ); // 提交晚归签到请求
+    return {
+      result: result.result, // 是否成功
+      message: result.msg, // 响应消息
+    };
+  }
 }
